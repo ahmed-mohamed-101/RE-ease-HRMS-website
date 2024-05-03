a242f61c9d704632e3315ff84f7039bdcf5f1ca0
unknown <you@example.com>
2024-05-03 15:20:47 +0200

    finaltoken

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PaymentService } from 'src/app/shared/services/payment.service';
 import { DomSanitizer } from '@angular/platform-browser';
 
@@ -7,8 +7,10 @@ import { DomSanitizer } from '@angular/platform-browser';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 constructor(private _PaymentService:PaymentService,private sanitizer: DomSanitizer){}
+
+
 
 handlepaymentannually():void{
   this._PaymentService.checkoutannually().subscribe({
@@ -32,6 +34,9 @@ handlepaymentmonthly():void{
 email: any;
 
 ngOnInit() {
+  // localStorage.setItem('etoken', 'nmbvhmv');
+  // console.log(localStorage.getItem('etoken'))
+  console.log(this._PaymentService.token)
   const email = 'RealeStateEase@gmail.com';
   this.email = this.sanitizer.bypassSecurityTrustHtml(email);
 }
--- front-end/src/app/shared/services/payment.service.ts
+++ front-end/src/app/shared/services/payment.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +14,35 @@ export class PaymentService {
 
   constructor(private _HttpClient:HttpClient ,private _Router:Router) { }
 
-  
+  // ngOnInit(): void {
+  //   console.log(this.usertoken)
+  // }
+
+  usertoken:any;
+
+  token:any ={token:localStorage.getItem('etoken')} ;
+
+
+//  saveusertoken(){
+//  if(localStorage.getItem('etoken') != null){
+
+//   let encodetoken:any =localStorage.getItem('etoken');
+
+//   let decodetoken= jwtDecode(encodetoken);
+
+//   this.usertoken=decodetoken;
+
+
+
+//  }
+//  }
 
   checkoutannually():Observable<any>{
-    return this._HttpClient.post(`http://localhost:3000/payment/annually`,
-    {token:localStorage.getItem('etoken')})     
+    return this._HttpClient.post(`http://localhost:3000/payment/annually`, this.token)     
   }
 
   checkoutmonthly():Observable<any>{
-    return this._HttpClient.post(`http://localhost:3000/payment/monthly`,
-    {token:localStorage.getItem('etoken')})
+    return this._HttpClient.post(`http://localhost:3000/payment/monthly`, this.token)
   }
 
 
