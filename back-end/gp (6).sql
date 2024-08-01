de95a9a8a00ee244ae659a165cc61e960deacd13
ahmed-mohamed-101 <am6665666@gmail.com>
2024-08-01 15:26:42 +0300

    add images

--- back-end/gp (6).sql
+++ back-end/gp (6).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 19, 2024 at 02:35 AM
+-- Generation Time: Jul 22, 2024 at 08:24 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -45,8 +45,7 @@ INSERT INTO `admins` (`id`, `name`, `email`, `password`, `company_name`, `is_adm
 (12, 'ahmed', 'ahmed@example.com', '$2a$12$HDdNpL6JqVqXkjqLRqBT0OVBKETTlFdrQvULKuCpiwumYFPANYGGi', 'company6', 1, 'cus_Q1Wf33earZCx1i'),
 (14, 'maya', 'maya@email.com', '$2a$12$YoKpMrA5AkLfcwL1NHRpjOIzPEdYjt2/Wt/vEZYDlKb0nF3PtnFNO', 'company8', 1, 'cus_Q1hBS7onPzyQpb'),
 (15, 'zyad', 'zyad@gmail.com', '$2a$12$xxY9EnRPrXJRdcVBG/kkwOymajCxDPzTuRRplNjm3oT2/ZcGbuFf.', 'REease', 1, 'cus_Q7QejoiSQTYhkC'),
-(16, 'ali mohamed', 'ali@gmail.com', '$2a$12$nBLapbd25FnC9dooF6FlWerpqPdcNxc2eEGeUjmUt/YH7DWfoKhKO', 'pheniox', 1, NULL),
-(17, 'maya', 'maya@email.com', '$2a$12$jOpAqFH0vNtrpImm9j62U.bIewx7t/Mp3QA42iRtZJ49ckMiIDmmq', 'company8', 1, NULL),
+(16, 'ali mohamed', 'ali@gmail.com', '$2a$12$nBLapbd25FnC9dooF6FlWerpqPdcNxc2eEGeUjmUt/YH7DWfoKhKO', 'pheniox', 1, 'cus_QLJI6QtIR3RAlF'),
 (18, 'mohsen', 'm@gmail.com', '$2a$12$meZYQTwqi0kk7QrAmSUhyusfVCzfcBkbmdfg22D2uQQGMXO4KvzXO', 'google', 1, NULL),
 (19, 'mahmoud mohamed', 'mahmoud@gmail.com', '$2a$12$RW1O2eVJmal/LefGgZ5aOOBFPkALIucKFeWBWt1xDIUy27VWYXOD.', 'company5555', 1, NULL),
 (20, 'omar ahmed', 'omar@gmail.com', '$2a$12$bCBQvyHW0qANUtuFwZQJce.jX9WVg3DDBXX4PhGO3GFimbxeRVbFS', 'company99999999', 1, NULL),
@@ -56,7 +55,13 @@ INSERT INTO `admins` (`id`, `name`, `email`, `password`, `company_name`, `is_adm
 (25, 'shahd', 'shahd@gmail.com', '$2a$12$oQ.hTu6ETo5q6FBnY3oTW.tXEZ/HPO9UEE8Qo87gJ9uB/Jpz0xKkq', 'puppyvet', 1, 'cus_QFDPMtt9G87jzS'),
 (32, 'kaya1', 'kaya1@gmail.com', '$2a$12$yE7BQeZMqAszhWqeQfXnAOphAwSZTlZecVmsCXLDWqJAv7dqWkrjO', 'company555', 1, 'cus_QFguX7Au5YELtd'),
 (33, 'shawky', 'shawky@gmail.com', '$2a$12$bEWTIme6CSbU2NdJf8GMgOXHSmoih5JQKD.OEAhFoFwq4FNBSkeO.', 'xxx', 1, 'cus_QHBgn9Y5wkV4z3'),
-(34, 'mamad', 'mamad@gmail.com', '$2a$12$gsY.2LJVxyLwbfFhlWj/buiMNztGhKGvT55tW.gNhB3XhLv2Ny/Gq', 'safasfafa', 1, NULL);
+(34, 'mamad', 'mamad@gmail.com', '$2a$12$gsY.2LJVxyLwbfFhlWj/buiMNztGhKGvT55tW.gNhB3XhLv2Ny/Gq', 'safasfafa', 1, 'cus_QKbH45fWEobhNg'),
+(35, 'ramy', 'ramy@gmail.com', '$2a$12$SWV/HtoaAXQp9RK70arFbebGM82d.1LzSQCa3ROywY5VLIwYvAit.', 'ramysssssss', 1, 'cus_QKcvGDHzzGfYRF'),
+(36, 'ahmed ali', 'ahmedali@gmail.com', '$2a$12$r0HKwG3JvqmNtAY4QzJG/uLLAfZxLm9R2eg.qJVvr/TP2HAkG3J.i', 'elhamd', 1, 'cus_QKpbQqm07zNFrT'),
+(37, 'Dina Sobhy', 'dinasobhy20@gmail.com', '$2a$12$2eS19/5DCn.e.jgRQg8GDOxx7nseQOQNy9I9s.n0MfsML5VrliP5q', 'RE', 1, 'cus_QOdFFHB45WPjet'),
+(38, 'marry', 'marry@gmail.com', '$2a$12$2jAquujtndApsWRB/yNKbu2/pQLpVf9ospkNy0INtOQNqlfx3Osyy', 'asasasaasas', 1, 'cus_QLquL2vAmvGLsD'),
+(39, 'yasmeensamuel', 'yasmeen20@gmail.com', '$2a$12$g6lTPr77l83Jpp5OeTXO.uzoJRCik0VYWyWyZ4e.ZWGTe.tyFaSR.', 'RE', 1, 'cus_QLr0rvHAYVztEW'),
+(40, 'marawan', 'marawan@gmail.com', '$2a$12$MUP8Wiwsrs.PkIeKrFc8VeFWDukKG/5ZEeFoT1uD5z.dGYhj6D0YC', 'company999', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -80,12 +85,13 @@ CREATE TABLE `attendance` (
 --
 
 INSERT INTO `attendance` (`id`, `email`, `date`, `clock_in`, `in_status`, `clock_out`, `out_status`, `company_name`) VALUES
-(1, 'mahdy@gmail.com', '4-6-2024', '9:0', 'on time', '17:0', 'on time', 'REease'),
+(1, 'mahdy@gmail.com', '4-6-2024', '9:0', 'on time', '17:0', 'late', 'REease'),
 (2, 'mahdy@gmail.com', '11-6-2024', '9:0', 'on time', '17:0', 'on time', 'REease'),
-(3, 'khaled@gmail.com', '11-17-2024', '9:0', 'on time', '20:0', 'extra', 'REease'),
 (6, 'khaled@gmail.com', '17-6-2024', '8:0', 'on time', '17:0', 'extra', 'REease'),
 (7, 'khaled@gmail.com', '11-6-2024', '10:0', 'absent', '20:0', 'extra', 'REease'),
-(8, 'khaled@gmail.com', '19-6-2024', '8:0', 'on time', '17:0', 'on time', 'REease');
+(8, 'khaled@gmail.com', '19-6-2024', '8:0', 'on time', '17:0', 'on time', 'REease'),
+(9, 'khaled@gmail.com', '21-6-2024', '8:0', 'on time', '17:0', 'on time', 'REease'),
+(13, 'khaled@gmail.com', '1-7-2024', '8:0', 'on time', '17:0', 'on time', 'REease');
 
 -- --------------------------------------------------------
 
@@ -109,12 +115,24 @@ CREATE TABLE `leaves` (
 --
 
 INSERT INTO `leaves` (`id`, `name`, `leave_type`, `start_date`, `end_date`, `description`, `status`, `company_name`) VALUES
-(5, 'mahdy', 'medical leave', '3-6-2024', '5-6-2024', 'iam very sick and i cant come to work', 'rejected', 'REease'),
 (6, 'mahdy', 'other', '3-6-2024', '5-6-2024', 'iam very sick and i cant come to work', 'approved', 'REease'),
-(7, 'mahdy', 'othersssssssss', '3-6-2024', '5-6-2024', 'iam very sick and i cant come to work', 'rejected', 'REease'),
-(10, 'khaled', 'medical leave', '12-6-2024', '15-6-2024', 'iam very sick and i cant come to work', 'rejected', 'REease'),
-(12, 'khaled', 'Distinctio Culpa re', '2017-07-12', '1992-09-21', 'Quo voluptatum odit ', 'pending', 'REease'),
-(13, 'khaled', 'Autem cillum volupta', '2012-11-05', '2002-02-18', 'Quia animi ullamco ', 'pending', 'REease');
+(7, 'mahdy', 'othersssssssss', '3-6-2024', '5-6-2024', 'iam very sick and i cant come to work', 'approved', 'REease'),
+(10, 'khaled', 'medical leave', '12-6-2024', '15-6-2024', 'iam very sick and i cant come to work', 'approved', 'REease'),
+(12, 'khaled', 'Distinctio Culpa re', '2017-07-12', '1992-09-21', 'Quo voluptatum odit ', 'approved', 'REease');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(255) NOT NULL,
+  `payment_amount` int(255) NOT NULL,
+  `payment_date` date NOT NULL,
+  `payment_email` varchar(255) NOT NULL,
+  `canrd_number` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -144,10 +162,16 @@ CREATE TABLE `payroll` (
 --
 
 INSERT INTO `payroll` (`id`, `name`, `position`, `company_name`, `basic`, `absent_attendance`, `extra_attendance`, `done_RE`, `allowance`, `deduction`, `total`, `month`, `year`, `status`) VALUES
-(2, 'mohamed', 'frontend developer', 'REease', 15000, 750, 281, 1000, 300, 400, 15431, 7, 2024, 'not paid'),
-(3, 'khaled', 'frontend developer', 'REease', 15000, 750, 281, 1000, 300, 400, 15431, 6, 2024, 'not paid'),
-(4, 'khaled', 'frontend developer', 'REease', 15000, 750, 281, 1000, 300, 550, 15281, 6, 2024, 'paid'),
-(5, 'khaled', 'frontend developer', 'REease', 15000, 750, 188, 1000, 300, 550, 15188, 6, 2024, 'not paid');
+(5, 'khaled', 'frontend developer', 'REease', 15000, 750, 188, 1000, 300, 550, 15188, 6, 2024, 'paid'),
+(6, 'khaled', 'frontend developer', 'REease', 15000, 750, 188, 1500, 300, 550, 15688, 6, 2024, 'paid'),
+(11, 'khaled', 'frontend developer', 'REease', 15000, 750, 188, 1500, 0, 0, 15938, 6, 2024, 'paid'),
+(27, 'khaled', 'frontend developer', 'REease', 15000, 0, 0, 0, 300, 550, 14750, 7, 2024, 'paid'),
+(30, 'khaled', 'real estate agent', 'REease', 30000, 0, 0, 0, 300, 550, 29750, 7, 2024, 'not paid'),
+(31, 'khaled', 'realestate agent', 'REease', 30000, 0, 0, 0, 300, 550, 29750, 7, 2024, 'not paid'),
+(38, 'Laura Meadows', 'Ipsum porro praesen', 'REease', 30000, 30000, 0, 0, 0, 0, 0, 8, 2024, 'paid'),
+(39, 'Laura Meadows', 'Ipsum porro praesen', 'REease', 30000, 30000, 0, 0, 100, 0, 100, 8, 2024, 'not paid'),
+(40, 'Laura Meadows', 'Ipsum porro praesen', 'REease', 30000, 30000, 0, 0, 100, 200, -100, 8, 2024, 'not paid'),
+(41, 'Rana Kamal', 'system analyst', 'REease', 40000, 40000, 0, 0, 700, 200, 500, 6, 2024, 'not paid');
 
 -- --------------------------------------------------------
 
@@ -174,9 +198,10 @@ CREATE TABLE `re` (
 --
 
 INSERT INTO `re` (`id`, `owner`, `type`, `address`, `size`, `status`, `done_date`, `price`, `assigned_to`, `company_name`, `document`) VALUES
-(2, 'khaled', 'appartment', 'giza', 200, 'for sale', '16-6-2024', 150000, 'khaled', 'REease', NULL),
-(7, 'khaled', 'appartment', 'giza', 200, 'sold out', '16-6-2024', 150000, 'khaled', 'REease', NULL),
-(8, 'khaled', 'appartment', 'giza', 200, 'rented', '16-6-2024', 150000, 'khaled', 'REease', NULL);
+(19, 'john', 'appartment', 'cairo,egypt', 180, 'sold out', '1-7-2024', 1500000, 'khaled', 'REease', 'https://esign.com/wp-content/uploads/Apartment-Lease-Agreement-Template.png'),
+(20, 'mostafa', 'villa', 'zamalk', 700, 'sold out', '22-6-2024', 30000000, 'khaled', 'REease', 'https://esign.com/wp-content/uploads/Apartment-Lease-Agreement-Template.png'),
+(21, 'lila', 'house', 'cairo', 110, 'for sale', NULL, 200000, 'khaled', 'REease', 'https://images.unsplash.com/photo-1562240020-ce31ccb0fa7d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+(22, 'lila', 'house', 'cairo', 110, 'for sale', NULL, 200000, 'khaled', 'REease', 'https://images.unsplash.com/photo-1562240020-ce31ccb0fa7d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 -- --------------------------------------------------------
 
@@ -200,9 +225,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `company_name`, `position`, `salary`, `is_admin`) VALUES
-(71, 'shady1', 'shady1@gmail.com', '$2a$12$0VBRs1SmL2G2tcllljBym.FsPMvnlBWo/Ejr2xEBLHdvfivahu10S', 'REease', 'frontend developer', 15000, 0),
-(72, 'Luke Clayton', 'nytadyga@mailinator.com', '$2a$12$hplV0DnjgN3.aVcnsAoG/.YygXeRxIE8MiKaZE5xMGTSDXTkIO9.O', 'company555', 'Perspiciatis aliqui', 1, 0),
-(73, 'khaled', 'khaled@gmail.com', '$2a$12$89Y7lX6ZsxS5xaPIGg/FWOqnNZHJH6q54/8mzNVRMXxJWYa/u6aPe', 'REease', 'frontend developer', 15000, 0);
+(77, 'mohamed', 'mohamed@gmail.com', '$2a$12$QsyyTVP0afW9XxIUeSfAJeSeFxoR9vjnZ0F/uZDaHf8n7BlxKAIAK', 'REease', 'backend developer', 15000, 0),
+(78, 'lina', 'lina@gmail.com', '$2a$12$DZnGxDeKVGU0W0cQgJP9POikGjsO5UqNTNVsn9Z0BwOTGoxjej2g6', 'REease', 'realestate agent', 30000, 0),
+(87, 'khaled', 'khaled@gmail.com', '$2a$12$8Lun5ppYJag/89yYlavhHu17zjKMwvH8.Kpf1O5M9P.SzgWVljJ8O', 'REease', 'realestate agent', 30000, 0),
+(91, 'Rana Kamal', 'ranakamal@gmail.com', '$2a$12$ac16qAS0nWtfAVpMn0M3DePnlIqELskQtcPiphixYJEPDZYUQ1kuS', 'REease', 'system analyst', 40000, 0),
+(94, 'khaled23', 'khaled23@gmail.com', '$2a$12$RqcQE05.cMikxjIjd3J7Fe1uWKMCh6.lpDe3jXRh5lDQlyExQXPaC', 'REease', 'frontend developer', 15000, 0);
 
 --
 -- Indexes for dumped tables
@@ -225,6 +252,12 @@ ALTER TABLE `attendance`
 --
 ALTER TABLE `leaves`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
 
 --
 -- Indexes for table `payroll`
@@ -252,37 +285,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `leaves`
 --
 ALTER TABLE `leaves`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `payroll`
 --
 ALTER TABLE `payroll`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `re`
 --
 ALTER TABLE `re`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
